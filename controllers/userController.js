c3dd0e088335aa9a5a6df00050688bb3f2e60fd3
utsavKhatri <utsavk@zignuts.com>
2023-02-15 11:30:40 +0530

    fix env issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 import { createConnection } from 'mysql';
 
 export const connection = createConnection({
-    host:'15.206.7.200',
-    port:'3310',
-    user:'utsavkh',
-    password:'675915xiCD4QCw3Vqt63f',
-    database:'utsavkh'
+    // host:'15.206.7.200',
+    // port:'3310',
+    // user:'utsavkh',
+    // password:'675915xiCD4QCw3Vqt63f',
+    // database:'utsavkh'
 });
 
