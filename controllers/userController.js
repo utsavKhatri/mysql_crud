553153b0d0ee7d6823c49c209e9a710628294fd5
utsavKhatri <utsavk@zignuts.com>
2023-02-15 11:05:02 +0530

    sm changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 import { createConnection } from 'mysql';
 
 export const connection = createConnection({
-    host:'',
-    port:'',
-    user:'',
-    password:'',
-    database:''
+    host:'15.206.7.200',
+    port:'3310',
+    user:'utsavkh',
+    password:'675915xiCD4QCw3Vqt63f',
+    database:'utsavkh'
 });
 
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,48 +1,64 @@
-
 import { Router } from "express";
 import { connection } from "../controllers/userController.js";
 // import { query } from "../lib/db";
 const router = Router();
 /* GET home page. */
 
+router.get("/", (req, res) => {
+  connection.query("select * from test1 order by id desc", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("home", { title: "the home page", data: data });
+  });
+});
 
+router.get("/add", (req, res) => {
+  res.render("add", { title: "the home page" });
+});
 
+router.post("/add", (req, res) => {
+  const user = [req.body.name, req.body.email];
+  console.log(req.body);
 
+  // let str = "INSERT INTO test1 (name, email) VALUES"+ user
+  connection.query(
+    `INSERT INTO test1 (name, email) VALUES ("${req.body.name}","${req.body.email}")`,
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        res.render("add", {
+          title: "ADD PAge",
+          name: user.name,
+          email: user.email,
+        });
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
 
-router.get('/', (req,res)=>{
+router.get("/edit/(:id)", function (req, res, next) {
+  connection.query(
+    "SELECT * FROM customers WHERE id = " + req.params.id,
+    (err, rows) => {
+          if (err)
+              throw err;
+          // if user not found
+          else {
+              // if user found
+              // render to views/user/edit.ejs template file
+              res.render("customers/edit", {
+                  title: "Edit Customer",
+                  //data: rows[0],
+                  id: rows[0].id,
+                  name: rows[0].name,
+                  email: rows[0].email,
+              });
+          }
+      }
+  );
+});
 
-    connection.query('select * from test1 order by id desc',(err,data)=>{
-        if (err) {
-            console.log(err);
-        }
-        res.render('home',{title: "the home page", data: data})
-        
-    })
-})
-
-router.get('/add', (req,res)=>{
-    res.render('add',{title:"the home page"})
-})
-
-router.post('/add', (req,res)=>{
-    const user =[
-        req.body.name,
-        req.body.email
-    ]
-    console.log(req.body);
-
-
-    // let str = "INSERT INTO test1 (name, email) VALUES"+ user
-    connection.query(`INSERT INTO test1 (name, email) VALUES ("${req.body.name}","${req.body.email}")`, (err,data)=>{
-        if (err) {
-            console.log(err);
-            res.render('add', {
-                title: "ADD PAge", name:user.name, email:user.email
-            })
-        }
-        else{
-            res.redirect('/')
-        }
-    })
-})
 export default router;
